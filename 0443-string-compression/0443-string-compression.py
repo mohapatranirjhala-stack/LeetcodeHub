223a931cd73ee6ea9193fbed3746c64fe82ce97a
Nirjhala Mohapatra <mohapatranirjhala@gmail.com>
2026-07-15 08:45:39 +0530

    Time: 0 ms (100%), Space: 19.3 MB (87.41%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,14 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write = anchor = 0
+        for read, char in enumerate(chars):
+            if read + 1 == len(chars) or chars[read + 1] != char:
+                chars[write] = chars[anchor]
+                write += 1
+                if read > anchor:
+                    for digit in str(read - anchor + 1):
+                        chars[write] = digit
+                        write += 1
+                anchor = read + 1
+        return write
+        
